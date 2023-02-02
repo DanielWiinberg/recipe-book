a90a9b88e314b50f649afef154873552ea5247ea
DanielWiinberg <daniel.wiinberg@gmail.com>
2023-02-02 20:18:02 +0100

    ???

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Pasta', 250),
+    new Ingredient('Parmesan', 50),
+    new Ingredient('Eggs', 3)
+  ];
 }
