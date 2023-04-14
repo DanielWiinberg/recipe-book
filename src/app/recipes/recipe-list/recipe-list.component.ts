6fb487bdbcb20a785623d5caedb300040584a3f9
danieldanwii <daniel.wiinberg@norconsult.com>
2023-04-14 10:35:15 +0200

    Finished section 12: Routing

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -11,10 +12,17 @@ import { RecipeService } from '../recipe.service';
 export class RecipeListComponent implements OnInit {
   savedRecipes: Recipe[];
 
-  constructor(private recipeService: RecipeService){
+  constructor(
+    private recipeService: RecipeService,
+    private router: Router,
+    private route: ActivatedRoute){
   }
 
   ngOnInit(){
     this.savedRecipes = this.recipeService.getRecipes();
+  }
+
+  onNewRecipe(){
+    this.router.navigate(['new'], {relativeTo: this.route});
   }
 }
